a9a29d38cb62b3db4ea1e4e4b202641e3d31860a
MariamSulikashvili1 <116154200+MariamSulikashvili1@users.noreply.github.com>
2023-03-22 16:45:52 +0400

    Add files via upload
    Task 7

---
+++ Python-django-application/Book/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Person(models.Model):
+    SHIRT_SIZES = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    name = models.CharField(max_length=60)
+    shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+>>> p = Person(name="Fred Flintstone", shirt_size="L")
+>>> p.save()
+>>> p.shirt_size
+'L'
+>>> p.get_shirt_size_display()
+'Large'
